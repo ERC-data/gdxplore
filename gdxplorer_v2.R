1883bdcea4250b41e4dbd8088c52cc3255beca37
SaintlyVi <wiebke.toussaint@gmail.com>
2016-08-29 14:02:06 +0200

    Now fetches data from CKAN.

--- gdxplorer_v2.R
+++ gdxplorer_v2.R
@@ -1,17 +1,13 @@
-
 library(shiny)
 
-rdsfilepath = 'C:/Users/01425453/Google Drive/SATIM/R codes and outputs/SATIM General Outputs/processed_TestProjname_24Aug2016.rds'
+rdsfilepath = "http://energydata.uct.ac.za/dataset/1db4d94d-867e-4706-a26e-b4600fee594b/resource/395efb75-30ae-4df1-b080-b7b3aabdb4fa/download/processedtestprojname24aug2016.rds"
 
-
-N = length(gdxlist) 
 tmplist = list()
 pwrdf = data.frame()
 tradf = data.frame()
 clpricesdf = data.frame()
 varactdf = data.frame()
-
-tmplist = readRDS(rdsfilepath)
+tmplist = readRDS(gzcon(url(rdsfilepath)))
 
 print('Appending pivottable dataframe from each case...')
 #now have a list of lists - one list for each gdx
@@ -26,5 +22,5 @@ for (i in 1:n){
   #comdf = rbind(comdf, as.data.frame(tmplist[[i]][4]))
 }
 
-runApp(paste(workdir,'/ShinyApp/',sep =''))
-
+path = getwd()
+runApp(paste(path,'/ShinyApp/',sep =''))
