cd7a4943606058f339d3c191043e6e87ad4759d4
Bryce M <mccall.bryce@gmail.com>
2016-09-01 13:58:48 +0200

    checks for scen first
    *before running append

--- appendNewResults.R
+++ appendNewResults.R
@@ -5,11 +5,14 @@ library(gdxrrw)
 library(dplyr)
 library(XLConnect)
 
-newfilepath = 'alocation' #location of the new gdx to be processed and appended
-existfilepath = '' #location of the existing processed results rds file
+newfilepath = 'C:/Users/01425453/Desktop/Student R layout/GDXfiles/03REF14T.gdx' #location of the new gdx to be processed and appended
+newgdxname = '03REF14T'
+  
+existfilepath = rdsfilepath #location of the existing processed results rds file
 
-#main working directory for this file/script and colourcoding files etc. 
-workdir = 'C:/Users/01425453/Google Drive/SATIM/R codes and outputs/SATIM General Outputs/'
+
+#workdir should already be initiated
+#workdir = 'C:/Users/01425453/Google Drive/SATIM/R codes and outputs/SATIM General Outputs/'
 
 GAMS_lib_dir = 'C:/GAMS/win64/24.7'  # location of your GAMS main directory. 
 
@@ -20,9 +23,6 @@ igdx(GAMS_lib_dir)
 #LOAD FUNCTIONS
 source(paste(workdir,'extractResults_v5.R',sep =''))
 
-#======================process and extract results from the new gdx
-print('Processing gdx to append...')
-newgdxresults = processGDX(newfilepath,newgdxname)
 
 
 #======================get existing processed set of results and append new ones
@@ -32,14 +32,24 @@ existrds = readRDS(existfilepath)
 print('appending new processed result to existing one...')
 if(newgdxname %in% names(existrds)){
   #make sure there is no scenario/gdxname in the existing file already
-  msg = c("A scenario ",newgdxname,' ','already exists!!!')
+  msg = paste(c("A scenario ",newgdxname,' ','already exists!!!'),collapse = '')
   stop(msg)
   #####EXIT HERE
 }else{
-  #append new result
+  #======================process and extract results from the new gdx
+  print('Processing gdx to append...')
+  newgdxresults = processGDX(newfilepath,newgdxname)
+  
   newrds = existrds
-  newrds[newgdxname] = newgdxresults
+  newrds[[newgdxname]] = newgdxresults
   print('done appending new result to rds')
 }
+
+#saving
+print('saving RDS file')
+dt = format(Sys.time(), "%d%b%Y")
+rdsname = paste(paste(projname,'processed',sep = '_'),dt,sep ='_')
+saveRDS(newrds,paste(saverdspath,paste(rdsname,'.rds',sep = ''),sep=''))
+print('saving complete')
 
 
