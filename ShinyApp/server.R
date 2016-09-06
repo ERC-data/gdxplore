4ca72de1906ff4862f8c6db42167b9606c4453db
Bryce M <mccall.bryce@gmail.com>
2016-09-06 16:38:05 +0200

    new UI setup - sub tabs
    *results extraction updated for new shiny UI
    *new shiny UI has sub tabs

--- ShinyApp/server.R
+++ ShinyApp/server.R
@@ -15,6 +15,76 @@ shinyServer(function(input, output) {
  
   #prcgdxfilepath <- input$prcgdxfilepath
   #prcgdxfilepath <- prcgdxfilepath$datapath#get the path
+  output$pwr1pivottable <- renderRpivotTable({
+    rpivotTable(
+      pwr1,
+      rows=myrowlist,
+      col = mycollist,
+      aggregatorName=deflt_aggr,
+      inclusions = list(Case = list(myinclusion)),
+      vals=deflt_vals,
+      rendererName = deflt_view
+    )
+    
+  })
+  output$pwr2pivottable <- renderRpivotTable({
+    rpivotTable(
+      pwr2,
+      rows=myrowlist,
+      col = mycollist,
+      aggregatorName=deflt_aggr,
+      inclusions = list(Case = list(myinclusion)),
+      vals=deflt_vals,
+      rendererName = deflt_view
+    )
+    
+  })
+  output$pwr3pivottable <- renderRpivotTable({
+    rpivotTable(
+      pwr3,
+      rows=myrowlist,
+      col = mycollist,
+      aggregatorName=deflt_aggr,
+      inclusions = list(Case = list(myinclusion)),
+      vals=deflt_vals,
+      rendererName = deflt_view
+    )
+    
+  })
+  output$pwr4pivottable <- renderRpivotTable({
+    rpivotTable(
+      pwr4,
+      rows=myrowlist,
+      col = mycollist,
+      aggregatorName=deflt_aggr,
+      inclusions = list(Case = list(myinclusion)),
+      vals=deflt_vals,
+      rendererName = deflt_view
+    )
+    
+  })
+  output$resfpivottable <- renderRpivotTable({
+    rpivotTable(
+      res_flows,
+      rows=myrowlist,
+      col = mycollist,
+      aggregatorName=deflt_aggr,
+      inclusions = list(Case = list(myinclusion)),
+      vals=deflt_vals,
+      rendererName = deflt_view
+    )
+  })
+  output$rescpivottable <- renderRpivotTable({
+    rpivotTable(
+      res_cost,
+      rows=myrowlist,
+      col = mycollist,
+      aggregatorName=deflt_aggr,
+      inclusions = list(Case = list(myinclusion)),
+      vals=deflt_vals,
+      rendererName = deflt_view
+    )
+  })
   
   output$pwrpivottable <- renderRpivotTable({
     rpivotTable(
@@ -38,6 +108,29 @@ shinyServer(function(input, output) {
       rendererName = deflt_view
     )
     })
+  output$indfpivottable <- renderRpivotTable({
+    rpivotTable(
+      ind_flows,
+      rows=c('Subsubsector','Commodity_Name'),
+      col = mycollist,
+      aggregatorName=deflt_aggr,
+      inclusions = list(Case = list(myinclusion)),
+      vals= 'F_IN',
+      rendererName = deflt_view
+    )
+  })
+  output$indcpivottable <- renderRpivotTable({
+    rpivotTable(
+      ind_costs,
+      rows=c('Subsubsector','Commodity_Name'),
+      col = mycollist,
+      aggregatorName=deflt_aggr,
+      inclusions = list(Case = list(myinclusion)),
+      vals= 'F_IN',
+      rendererName = deflt_view
+    )
+  })
+  
   output$trapivottable <- renderRpivotTable({
     rpivotTable(
       tradf,
@@ -50,6 +143,104 @@ shinyServer(function(input, output) {
     )
     
   })
+  output$tracappivottable <- renderRpivotTable({
+    rpivotTable(
+      tra_cap,
+      rows=c('Subsector','Subsubsector','Commodity_Name'),
+      col = mycollist,
+      aggregatorName=deflt_aggr,
+      inclusions = list(Case = list(myinclusion)),
+      vals= 'Capacity',
+      rendererName = deflt_view
+    )
+    
+  })
+  output$trafpivottable <- renderRpivotTable({
+    rpivotTable(
+      tra_flows,
+      rows=c('Subsector','Subsubsector','Commodity_Name'),
+      col = mycollist,
+      aggregatorName=deflt_aggr,
+      inclusions = list(Case = list(myinclusion)),
+      vals= 'F_IN',
+      rendererName = deflt_view
+    )
+    
+  })
+  output$tracpivottable <- renderRpivotTable({
+    rpivotTable(
+      tra_costs,
+      rows=c('Subsector','Subsubsector','Commodity_Name'),
+      col = mycollist,
+      aggregatorName=deflt_aggr,
+      inclusions = list(Case = list(myinclusion)),
+      vals= 'CST_INVC',
+      rendererName = deflt_view
+    )
+    
+  })
+  output$trancappivottable <- renderRpivotTable({
+    rpivotTable(
+      tra_ncap,
+      rows=c('Subsector','Subsubsector','Commodity_Name'),
+      col = mycollist,
+      aggregatorName=deflt_aggr,
+      inclusions = list(Case = list(myinclusion)),
+      vals= 'NCAPL',
+      rendererName = deflt_view
+    )
+    
+  })
+  
+  output$refcappivottable <- renderRpivotTable({
+    rpivotTable(
+      refs_cap,
+      rows=c('Subsector','Subsubsector','Commodity_Name'),
+      col = mycollist,
+      aggregatorName=deflt_aggr,
+      inclusions = list(Case = list(myinclusion)),
+      vals= 'Capacity',
+      rendererName = deflt_view
+    )
+    
+  })
+  output$reffpivottable <- renderRpivotTable({
+    rpivotTable(
+      refs_flows,
+      rows=c('Subsector','Subsubsector','Commodity_Name'),
+      col = mycollist,
+      aggregatorName=deflt_aggr,
+      inclusions = list(Case = list(myinclusion)),
+      vals= 'F_IN',
+      rendererName = deflt_view
+    )
+    
+  })
+  output$refcpivottable <- renderRpivotTable({
+    rpivotTable(
+      refs_costs,
+      rows=c('Subsector','Subsubsector','Commodity_Name'),
+      col = mycollist,
+      aggregatorName=deflt_aggr,
+      inclusions = list(Case = list(myinclusion)),
+      vals= 'CST_INVC',
+      rendererName = deflt_view
+    )
+    
+  })
+  output$refncappivottable <- renderRpivotTable({
+    rpivotTable(
+      refs_ncap,
+      rows=c('Subsector','Subsubsector','Commodity_Name'),
+      col = mycollist,
+      aggregatorName=deflt_aggr,
+      inclusions = list(Case = list(myinclusion)),
+      vals= 'NCAPL',
+      rendererName = deflt_view
+    )
+    
+  })
+  
   output$respivottable <- renderRpivotTable({
     rpivotTable(
       resdf,
@@ -65,6 +256,31 @@ shinyServer(function(input, output) {
   output$compivottable <- renderRpivotTable({
     rpivotTable(
       comdf,
+      rows=c('Subsubsector','Commodity_Name'),
+      col = mycollist,
+      aggregatorName=deflt_aggr,
+      inclusions = list(Case = list(myinclusion)),
+      vals= 'F_IN',
+      rendererName = deflt_view
+    )
+    
+  })
+  
+  output$comfpivottable <- renderRpivotTable({
+    rpivotTable(
+      com_flows,
+      rows=c('Subsubsector','Commodity_Name'),
+      col = mycollist,
+      aggregatorName=deflt_aggr,
+      inclusions = list(Case = list(myinclusion)),
+      vals= 'F_IN',
+      rendererName = deflt_view
+    )
+    
+  })
+  output$comcpivottable <- renderRpivotTable({
+    rpivotTable(
+      com_costs,
       rows=c('Subsubsector','Commodity_Name'),
       col = mycollist,
       aggregatorName=deflt_aggr,
--- gdxplorer_v2.R
+++ gdxplorer_v2.R
@@ -6,15 +6,28 @@ library(shiny)
 #rdsfilepath = "http://energydata.uct.ac.za/dataset/1db4d94d-867e-4706-a26e-b4600fee594b/resource/395efb75-30ae-4df1-b080-b7b3aabdb4fa/download/processedtestprojname24aug2016.rds"
 
 #local:
-rdsfilepath = 'C:/Users/01425453/Desktop/Student R layout/RDSfiles/student_3scens_processed_02Sep2016.rds'
+rdsfilepath = 'C:/Users/01425453/Desktop/Student R layout/RDSfiles/student_3scens_processed_06Sep2016.rds'
 #'C:/Users/01425453/Google Drive/SATIM/R codes and outputs/SATIM General Outputs/processed_TestProjname_24Aug2016.rds'
 
 tmplist = list()
 pwrdf = data.frame()
-tradf = data.frame()
+pwr1 = pwr2 = pwr3 = pwr4 = data.frame()
+
+tradf = tra_flows = tra_costs = tra_cap = tra_ncap = data.frame()
+refs_costs = refs_flows = refs_ncap = refs_cap = data.frame()
+
 resdf = data.frame()
+res_flows = data.frame()
+res_cost = data.frame()
+
 inddf = data.frame()
+ind_costs = data.frame()
+ind_flows =data.frame()
+
 comdf  = data.frame()
+com_costs = data.frame()
+com_flows = data.frame()
+
 clpricesdf = data.frame()
 varactdf = data.frame()
 
@@ -31,12 +44,40 @@ n = length(tmplist)
 for (i in 1:n){
   print(i)
   pwrdf = rbind(pwrdf,as.data.frame(tmplist[[i]][1]))
-  tradf = rbind(tradf,as.data.frame(tmplist[[i]][2]))
-  clpricesdf = rbind(clpricesdf, as.data.frame(tmplist[[i]][3]))
-  varactdf = rbind(varactdf, as.data.frame(tmplist[[i]][4]))
-  inddf = rbind(inddf,as.data.frame(tmplist[[i]][5]))
-  resdf = rbind(resdf,as.data.frame(tmplist[[i]][6]))
-  comdf = rbind(comdf, as.data.frame(tmplist[[i]][7]))
+  pwr1 = rbind(pwr1,as.data.frame(tmplist[[i]][2]))
+  pwr2 = rbind(pwr2,as.data.frame(tmplist[[i]][3]))
+  pwr3 = rbind(pwr3,as.data.frame(tmplist[[i]][4]))
+  pwr4 = rbind(pwr4,as.data.frame(tmplist[[i]][5]))
+  
+  tradf = rbind(tradf,as.data.frame(tmplist[[i]][6]))
+  tra_flows = rbind(tra_flows,as.data.frame(tmplist[[i]][18]))
+  tra_costs = rbind(tra_costs,as.data.frame(tmplist[[i]][19]))
+  tra_cap = rbind(tra_cap,as.data.frame(tmplist[[i]][20]))
+  tra_ncap = rbind(tra_ncap,as.data.frame(tmplist[[i]][21]))
+  
+  
+  clpricesdf = rbind(clpricesdf, as.data.frame(tmplist[[i]][7]))
+  
+  varactdf = rbind(varactdf, as.data.frame(tmplist[[i]][8]))
+  
+  inddf = rbind(inddf,as.data.frame(tmplist[[i]][9]))
+  ind_flows = rbind(ind_flows,as.data.frame(tmplist[[i]][10]))
+  ind_costs = rbind(ind_costs,as.data.frame(tmplist[[i]][11]))
+  
+  
+  resdf = rbind(resdf,as.data.frame(tmplist[[i]][12]))
+  res_flows = rbind(res_flows,as.data.frame(tmplist[[i]][13]))
+  res_cost = rbind(res_cost,as.data.frame(tmplist[[i]][14]))
+  
+  comdf = rbind(comdf, as.data.frame(tmplist[[i]][15]))
+  com_costs = rbind(com_costs,as.data.frame(tmplist[[i]][16]))
+  com_flows = rbind(com_flows,as.data.frame(tmplist[[i]][17]))
+  
+  refs_flows = rbind(refs_flows,as.data.frame(tmplist[[i]][22]))
+  refs_costs = rbind(refs_costs,as.data.frame(tmplist[[i]][23]))
+  refs_cap = rbind(refs_cap,as.data.frame(tmplist[[i]][24]))
+  refs_ncap = rbind(refs_ncap,as.data.frame(tmplist[[i]][25]))
+  
 }
 
 #runApp(paste(getwd(),'/ShinyApp/',sep =''))
