f93be4430425a75fd5ff68bca214be66afc4e006
Bryce M <mccall.bryce@gmail.com>
2016-09-09 16:38:32 +0200

    this is a copy of gdxplorer - changed the name
    *also changed filepaths

---
+++ load_dataframes.R
@@ -0,0 +1,91 @@
+#read in the saved rds file for the extracted results
+#create all dataframes for the pivot tables
+#RUN the shiny viewer
+
+library(shiny)
+
+#get first processed rds file 
+thispath = 'C:/EMOD/Rfiles/'
+saverdspath = 'C:/EMOD/RDSfiles/'
+setwd(saverdspath)
+
+rdslist=list.files(pattern=".rds")
+rdsfilepath = paste('C:/EMOD/RDSfiles',rdslist[1],sep = '/')
+
+tmplist = list()
+pwrdf = data.frame()
+pwr_cap = pwr_ncap = pwr_flows = pwr_costs = data.frame()
+
+tradf = tra_flows = tra_costs = tra_cap = tra_ncap = data.frame()
+refs_costs = refs_flows = refs_ncap = refs_cap = data.frame()
+pwr_emis = ind_emis = res_emis = com_emis = tra_emis = sup_emis= refs_emis= all_emis = data.frame()
+
+resdf = data.frame()
+res_flows = data.frame()
+res_cost = data.frame()
+
+inddf = data.frame()
+ind_costs = data.frame()
+ind_flows =data.frame()
+
+comdf  = data.frame()
+com_costs = data.frame()
+com_flows = data.frame()
+
+clpricesdf = data.frame()
+varactdf = data.frame()
+
+#local 
+tmplist = readRDS(rdsfilepath)
+
+print('Loading dataframes for pivot tables...')
+#now have a list of lists - one list for each gdx
+#now need to take each df out of each gdx list and append to masterdf's for each subsector
+n = length(tmplist)
+for (i in 1:n){
+  print(i)
+  pwrdf = rbind(pwrdf,as.data.frame(tmplist[[i]][1]))
+  pwr_cap = rbind(pwr_cap,as.data.frame(tmplist[[i]][2]))
+  pwr_ncap = rbind(pwr_ncap,as.data.frame(tmplist[[i]][3]))
+  pwr_flows = rbind(pwr_flows,as.data.frame(tmplist[[i]][4]))
+  pwr_costs = rbind(pwr_costs,as.data.frame(tmplist[[i]][5]))
+  
+  tradf = rbind(tradf,as.data.frame(tmplist[[i]][6]))
+  tra_flows = rbind(tra_flows,as.data.frame(tmplist[[i]][18]))
+  tra_costs = rbind(tra_costs,as.data.frame(tmplist[[i]][19]))
+  tra_cap = rbind(tra_cap,as.data.frame(tmplist[[i]][20]))
+  tra_ncap = rbind(tra_ncap,as.data.frame(tmplist[[i]][21]))
+  
+  clpricesdf = rbind(clpricesdf, as.data.frame(tmplist[[i]][7]))
+  
+  varactdf = rbind(varactdf, as.data.frame(tmplist[[i]][8]))
+  
+  inddf = rbind(inddf,as.data.frame(tmplist[[i]][9]))
+  ind_flows = rbind(ind_flows,as.data.frame(tmplist[[i]][10]))
+  ind_costs = rbind(ind_costs,as.data.frame(tmplist[[i]][11]))
+  
+  resdf = rbind(resdf,as.data.frame(tmplist[[i]][12]))
+  res_flows = rbind(res_flows,as.data.frame(tmplist[[i]][13]))
+  res_cost = rbind(res_cost,as.data.frame(tmplist[[i]][14]))
+  
+  comdf = rbind(comdf, as.data.frame(tmplist[[i]][15]))
+  com_costs = rbind(com_costs,as.data.frame(tmplist[[i]][16]))
+  com_flows = rbind(com_flows,as.data.frame(tmplist[[i]][17]))
+  
+  refs_flows = rbind(refs_flows,as.data.frame(tmplist[[i]][22]))
+  refs_costs = rbind(refs_costs,as.data.frame(tmplist[[i]][23]))
+  refs_cap = rbind(refs_cap,as.data.frame(tmplist[[i]][24]))
+  refs_ncap = rbind(refs_ncap,as.data.frame(tmplist[[i]][25]))
+  
+  pwr_emis = rbind(pwr_emis,as.data.frame(tmplist[[i]][26]))
+  ind_emis = rbind(ind_emis,as.data.frame(tmplist[[i]][27]))
+  res_emis = rbind(res_emis,as.data.frame(tmplist[[i]][28]))
+  com_emis = rbind(com_emis,as.data.frame(tmplist[[i]][29]))
+  tra_emis = rbind(tra_emis,as.data.frame(tmplist[[i]][30]))
+  sup_emis = rbind(sup_emis,as.data.frame(tmplist[[i]][31]))
+  refs_emis = rbind(refs_emis,as.data.frame(tmplist[[i]][32]))
+  all_emis = rbind(all_emis,as.data.frame(tmplist[[i]][33]))
+}
+print('...Dataframes loaded')
+shiny::runApp(paste(thispath,'ShinyApp/',sep =''),launch.browser = TRUE)
+
