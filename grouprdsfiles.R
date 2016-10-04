34b47b65704869c4ca5dce7a19a8215210906ad4
Bryce M <mccall.bryce@gmail.com>
2016-10-04 16:57:28 +0200

    deleted something

--- grouprdsfiles.R
+++ grouprdsfiles.R
@@ -3,8 +3,6 @@
 groupfiles = function (rdslist){ #the character vec of rds files to group
   n = length(rdslist)
   
-  source('C:/EMOD/Rfiles/appendNewResults.R')#load the append new result function 
-  
   for (i in seq(1,n)){#loop over each rds file and append it
     
     appendRDSfile(rdslist[i])#this is from the appendNewResults.R script  
