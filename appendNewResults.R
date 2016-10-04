47f8b160fa7c65954de01963fab3165d6f3fcdfd
Bryce M <mccall.bryce@gmail.com>
2016-10-04 16:10:44 +0200

    INTGRATING TO User Shiny selections
    *This is incorporated into a shiny app which allows user to select which ones they want to view.

--- appendNewResults.R
+++ appendNewResults.R
@@ -1,16 +1,21 @@
-#take newscenario.rds and append to existing_processed_scenarios.rds main list 
+#take newscenario.rds and append to scenarios_processed.rds main list 
+#this is now a function
 
-  #GET NAME OF RDS FILE FROM COMMAND PROMPT
-  cmd_rdsname <- commandArgs(trailingOnly = TRUE) #this must be the file name eg: 01REF (without the .rds extension)
-  newrdsname = cmd_rdsname[1]
 
+#  #GET NAME OF RDS FILE FROM COMMAND PROMPT
+#  cmd_rdsname <- commandArgs(trailingOnly = TRUE) #this must be the file name eg: 01REF (without the .rds extension)
+# newrdsname = cmd_rdsname[1]
+
+
+appendRDSfile <- function(newrdsname){
+print(paste('Appending ',newrdsname,sep =' '))
 rdsfileslocation = 'C:/EMOD/RDSfiles'
 newrdsfilename = paste(newrdsname,'.rds',sep = '') #file name of the new results. example: 01REF.rds 
 
 newrdsfilepath= paste(rdsfileslocation,newrdsfilename,sep = '/')
 
 details = file.info(list.files(rdsfileslocation,pattern="*.rds"))
-existfilename = rownames(details)[grepl('processed',rownames(details))] #get the most recent modified grouped results rds file to append to
+existfilename = rownames(details)[grepl('grouped_scenarios',rownames(details))] #get the most recent modified grouped results rds file to append to
 existfilelocation = paste(rdsfileslocation,existfilename,sep = '/')
 
 if (file.exists(existfilelocation)){
@@ -20,7 +25,7 @@ if (file.exists(existfilelocation)){
   #if there isnt an rds file with 'processed' in the name in the path then make a blank list
   print('Creating blank rds file to append to.')
   existrds = list()
-  existfilelocation = paste(existfilelocation,'scenarios_processed.rds',sep = '/')#file doesnt exist. so make up the new name to save to at the end
+  existfilelocation = paste(existfilelocation,'grouped_scenarios.rds',sep = '/')#file doesnt exist. so make up the new name to save to at the end
 }
 
 
@@ -38,5 +43,5 @@ print('saving RDS file')
 saverdspath = existfilelocation
 saveRDS(newrds,saverdspath)
 print('saving complete')
-
+}
 
