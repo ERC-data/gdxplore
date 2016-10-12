1533f4688202b98087b440d7c9b6cf13da171762
Bryce M <mccall.bryce@gmail.com>
2016-10-12 12:44:31 +0200

    Removed 'appendResults' as it complicated things
    *this now does the job of that script and simpler

--- grouprdsfiles.R
+++ grouprdsfiles.R
@@ -2,12 +2,19 @@
 
 groupfiles = function (rdslist){ #the character vec of rds files to group
   n = length(rdslist)
-  
-  for (i in seq(1,n)){#loop over each rds file and append it
+  newrds = list() # create blank empty main list that we will append results for viewing to. 
+  print('appending results...')
+  for (i in seq(1,n)){#loop over each rds file and append it to newrds
+    print(rdslist[i])
+    newrdsfilename = paste(rdslist[i],'.rds',sep = '') #file name of the new results. example: 01REF.rds 
+    newrdsfilepath= paste('C:/EMOD/RDSfiles',newrdsfilename,sep = '/')
+    newgdxresults = readRDS(newrdsfilepath)
     
-    appendRDSfile(rdslist[i])#this is from the appendNewResults.R script  
-    
+    newrds[newrdsname] = newgdxresults #append, and give it the same name as the scenario name
     
   } 
+  print('saving RDS file')
+  saverdspath = 'C:/EMOD/RDSfiles/grouped_scenarios.rds'
+  saveRDS(newrds,saverdspath)
 
 }
