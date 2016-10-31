4aeecb4b4e9183d55f4b286b19bca7a334872acf
SaintlyVi <wiebke.toussaint@gmail.com>
2016-10-31 18:55:30 +0200

    modified master to read data from CKAN with ckanr

--- grouprdsfiles.R
+++ grouprdsfiles.R
@@ -1,20 +1,18 @@
+library(ckanr) #for data portal
+library(SOAR) #for caching
+
 # loops over all rds files that the user has chosen and combines them for the viewer. 
 
 groupfiles = function (rdslist){ #the character vec of rds files to group
-  n = length(rdslist)
   newrds = list() # create blank empty main list that we will append results for viewing to. 
   
-  for (i in seq(1,n)){#loop over each rds file and append it to newrds
-    print(rdslist[i])
-    newrdsfilename = paste(rdslist[i],'.rds',sep = '') #file name of the new results. example: 01REF.rds 
-    newrdsfilepath= paste('C:/EMOD/RDSfiles',newrdsfilename,sep = '/')
-    newgdxresults = readRDS(newrdsfilepath)
-    newrdsname = rdslist[i] #give it the same name as the scenario
-    newrds[newrdsname] = newgdxresults #append, and give it the same name as the scenario name
-    
-  } 
-  print('saving RDS file')
-  saverdspath = 'C:/EMOD/RDSfiles/grouped_scenarios.rds'
-  saveRDS(newrds,saverdspath)
-
+  for (r in rdslist){ #read each resource from CKAN
+      print(r)
+      resource_url <- resource_search(q = paste('name:', r, sep=''), as = 'table')$results$url #ckanr API call
+      newgdxresults <- readRDS(gzcon(url(resource_url)))
+      newrds[r] = newgdxresults #append, and give it the same name as the scenario name
+    }
+  
+  print('caching RDS file')
+  Store(newrds)
 }
