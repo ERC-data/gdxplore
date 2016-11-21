2210f8abe5fb6e59998eac4b4fbd75f9b266877e
SaintlyVi <wiebke.toussaint@gmail.com>
2016-11-21 10:28:36 +0200

    removed install.packages for shinyapps.io upload

--- ShinyApp/setup.R
+++ ShinyApp/setup.R
@@ -1,13 +1,5 @@
 # Install and load required packages
-packages <- c('ckanr','SOAR','shiny','rpivotTable','rprojroot')
-for (p in packages){
-    if (p %in% rownames(installed.packages())){
-        lapply(p, library, character.only = TRUE)
-    } else {
-        install.packages(p)
-        lapply(p, library, character.only = TRUE)
-    }
-}
+library(c('ckanr','SOAR','shiny','rpivotTable','rprojroot'))
 
 # Local variables
 path <- getwd()
